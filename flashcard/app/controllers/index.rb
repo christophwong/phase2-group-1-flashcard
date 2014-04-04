7bef6a1255eb86f82ef0bd83537ef866de64a417
Jarrod Scott <jarrodscott@Jarrods-MacBook-Pro.local>
2014-04-04 14:41:47 -0500

    altered homepage with title and deck link list. Also added a deck to seed file.

--- flashcard/app/controllers/index.rb
+++ flashcard/app/controllers/index.rb
@@ -1,4 +1,5 @@
 get '/' do
   # Look in app/views/index.erb
+  @list_of_decks = Deck.all 
   erb :index
 end
--- flashcard/db/seeds.rb
+++ flashcard/db/seeds.rb
@@ -1,4 +1,10 @@
 Deck.create(topic: "Artists and Albums")
+Deck.create(topic: "States and Capitals")
+
 Card.create(deck_id: 1, question: "Revolver", answer: "The Beatles")
 Card.create(deck_id: 1, question: "The Heist", answer: "Macklemore")
 Card.create(deck_id: 1, question: "Phyical Graffiti", answer: "Led Zeppelin")
+
+Card.create(deck_id: 2, question: "Ohio", answer: "Columbus")
+Card.create(deck_id: 2, question: "Virginia", answer: "Richmond")
+Card.create(deck_id: 2, question: "New York", answer: "Albany")

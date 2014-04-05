a01773807d28361ad0471021c3d4a3234618b66c
Jarrod Scott <jarrodscott@Jarrods-MacBook-Pro.local>
2014-04-04 20:22:24 -0500

    account creation, login, and log out implementeted - need validtions

--- flashcard/app/controllers/index.rb
+++ flashcard/app/controllers/index.rb
@@ -1,5 +1,49 @@
+enable 'sessions'
+
 get '/' do
   # Look in app/views/index.erb
-  @list_of_decks = Deck.all 
+  
   erb :index
 end
+
+
+post '/new_user' do  #new_user
+  if params[:password] == params[:confirmation]
+
+    @user = User.create!(name: params[:name], email: params[:email], password: params[:password])
+    session[:value] = @user.id
+    p session.inspect
+    @list_of_decks = Deck.all 
+    erb :decks
+
+  else
+
+    @error = true
+    erb :index
+  
+  end
+end
+
+post '/decks' do
+  u = User.find_by(email: params[:email])
+
+
+  if params[:password] == u.password
+
+    @list_of_decks = Deck.all
+    erb :decks
+
+  else
+
+    erb :index
+
+  end
+end
+
+get '/logout' do
+  session[:value] = nil
+  p session.inspect
+  redirect to '/'
+end
+
+

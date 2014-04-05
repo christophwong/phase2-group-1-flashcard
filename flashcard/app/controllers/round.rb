47f0a15bdb91c904e71b368c893986d8d36023ee
Christoph Wong <wong.christoph@gmail.com>
2014-04-05 15:55:59 -0500

    you can now play normally

--- flashcard/app/controllers/round.rb
+++ flashcard/app/controllers/round.rb
@@ -2,11 +2,16 @@ get '/round/:id' do
  @round = Round.find(params[:id])
  @deck = Deck.find(@round.deck_id)
  @cards = @deck.cards
- counter = 1
+ counter = @cards.first.id
  while Guess.where(card_id: counter).first != nil do
   counter += 1
  end
-  @card = @cards.find(counter)
-
-  erb :card
+  if counter > @cards.last.id
+    @list_of_decks = Deck.all
+    Guess.destroy_all
+    erb :decks
+  else
+    @card = @cards.find(counter)
+    erb :card
+  end
 end
